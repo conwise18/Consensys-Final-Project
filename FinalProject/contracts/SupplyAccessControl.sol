27fe03774aabb5d016923f6da045d3d0c0dd3751
conwise18 <33127302+conwise18@users.noreply.github.com>
2018-08-26 04:13:03 +1000

    Update SupplyAccessControl.sol

--- FinalProject/contracts/SupplyAccessControl.sol
+++ FinalProject/contracts/SupplyAccessControl.sol
@@ -1,12 +1,25 @@
 pragma solidity ^0.4.23;
 
+// ----------------------------------------------------------------------------
+// ConsenSys Academy’s 2018 Developer Program Final Project
+//
+// https://github.com/conwise18/Consensys-Final-Project
+//
+// Enjoy! (c) Connor Wiseman / The MIT Licence.
+// ----------------------------------------------------------------------------
+
+
+// Contract declaration
 contract SupplyAccessControl {
     
+    // State variables
     address public CEO;
     address public CTO;
     address public COO;
     address public CFO;
+
     
+    // Modifier functions
     modifier onlyCEO() {
         require (msg.sender == CEO);
         _;
@@ -35,29 +48,36 @@ contract SupplyAccessControl {
         _;
     }
     
+
+    // Constructor function
     constructor () public {
         CEO = msg.sender;
     }
     
-    function addCTO(address _newCTO) public {
+    // Function add new CTO
+    function addCTO(address _newCTO) public onlyCLevel {
         require (_newCTO != address(0));
         CTO = _newCTO;
     }
     
+    // Function add new COO
     function addCOO(address _newCOO) public {
         require (_newCOO != address(0));
         COO = _newCOO;
     }
     
+    // Function add new CFO
     function addCFO(address _newCFO) public {
         require (_newCFO != address(0));
         CFO = _newCFO;
     }
     
+    // Fallback function
     function () external payable {
         
     }
     
+    // Function allows to withdraw contract funds
     function withdraw() public onlyCEO {
         CEO.transfer(address(this).balance);
     }
